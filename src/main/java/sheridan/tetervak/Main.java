b2f62e29d7766a29f5342a2ff7ac126f508491de
Alex Tetervak <tetervak@sheridancollege.ca>
2024-01-09 11:53:03 -0500

    edited print in Main

--- src/main/java/sheridan/tetervak/Main.java
+++ src/main/java/sheridan/tetervak/Main.java
@@ -5,6 +5,8 @@ import static java.lang.System.out;
 public class Main {
 
     public static void main(String[] args) {
-        out.println("Hello world!");
+        out.println("Hello from MyDice1!");
+
+        out.println("See you later!");
     }
 }
