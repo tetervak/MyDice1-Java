7c23338971de54cc6e677a19e488597fc75feb4b
Alex Tetervak <tetervak@sheridancollege.ca>
2024-01-09 10:49:10 -0500

    initial commit

---
+++ src/main/java/sheridan/tetervak/Main.java
@@ -0,0 +1,10 @@
+package sheridan.tetervak;
+
+import static java.lang.System.out;
+
+public class Main {
+
+    public static void main(String[] args) {
+        out.println("Hello world!");
+    }
+}
